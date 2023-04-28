cb95dda7240489c5f5ff0ac0e13693f69f5fd462
Shree <shree.s.jogi@gmail.com>
2023-04-28 11:23:05 +0530

    complete

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import SocialIcons from "../components/SocialIcons";
 import { useInView } from "react-intersection-observer";
 import { useState, useEffect } from "react";
-import resume from "../pages/about/michael-yeates-resume.pdf";
+import resume from "../pages/about/Java-Servlet-Tutorial.pdf";
 
 const AboutMe = ({ name, email, location, availability, brand }) => {
   const [ref, inView] = useInView({
@@ -21,7 +21,7 @@ const AboutMe = ({ name, email, location, availability, brand }) => {
     setDownloading(true);
     const link = document.createElement("a");
     link.href = resume;
-    link.download = "Michael-Yeates-Resume.pdf";
+    link.download = "Java-Servlet-Tutorial.pdf";
     link.onload = () => {
       link.remove();
       setDownloading(false);
@@ -40,7 +40,7 @@ const AboutMe = ({ name, email, location, availability, brand }) => {
           animate={inView ? { x: 0, opacity: 1 } : { x: "-10vw", opacity: 0 }}
           transition={{ duration: 0.4, ease: "easeInOut" }}
         >
-          <img src={aboutMeImg} alt={name} />
+          
         </motion.div>
         <motion.div
           className="personalInfo col-12 col-lg-8"
--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -10,17 +10,17 @@ const SocialIcons = () => {
 
   return (
     <div className="socialIcons" style={styles.socialIcons}>
-      <a className="icon" style={styles.icon} href="https://github.com/mdyeates">
-        <i className="fa-brands fa-github" aria-hidden="true" title="Michael Yeates' GitHub Profile"></i>
+      <a className="icon" style={styles.icon} href="https://github.com/Shree-J">
+        <i className="fa-brands fa-github" aria-hidden="true" title="Shree Jogi' GitHub Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/mdyeates/">
-        <i className="fa-brands fa-linkedin" aria-hidden="true" title="Michael Yeates' LinkedIn Profile"></i>
+      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/shree-sanjay-jogi-369985260/">
+        <i className="fa-brands fa-linkedin" aria-hidden="true" title="Shree Jogi' LinkedIn Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://www.instagram.com/michael.yeates/">
-        <i className="fa-brands fa-instagram" aria-hidden="true" title="Michael Yeates' Instagram Profile"></i>
+      <a className="icon" style={styles.icon} href="">
+        <i className="fa-brands fa-instagram" aria-hidden="true" title="Shree' Instagram Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://twitter.com/mdyeates">
-        <i className="fa-brands fa-twitter" aria-hidden="true" title="Michael Yeates' Twitter Profile"></i>
+      <a className="icon" style={styles.icon} href="">
+        <i className="fa-brands fa-twitter" aria-hidden="true" title="Shree' Twitter Profile"></i>
       </a>
     </div>
   );
